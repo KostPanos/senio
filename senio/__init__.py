7bc25d47a2598f704e7062cc6383c6d58afa2deb
KostPanos <kost929pan@gmail.com>
2023-09-19 23:13:36 +0300

    Initial commit

---
+++ senio/__init__.py
@@ -0,0 +1,9 @@
+from senio.processors import processor_all, processor_rgbn
+from senio.utils import (
+    __version__,
+    calculate_statistics,
+    check_path_validity,
+    create_pam_xml,
+    setup_logger,
+    stack_sort,
+)
